2e23f23fd65286968ddf28aa986def97adebbc33
MatthiasBenaets <matthias.benaets@gmail.com>
2021-10-08 22:36:39 +0200

    Customization

--- debian-dwm.sh
+++ debian-dwm.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-#apt install git and run as root
-
+#Before starting the script
+#Apt install git and run as root
 echo "What is your username? [case sensitive]"
 read user
 
+#Update and install essentials
+#Add user to sudoers
 apt-get update -y
 apt-get upgrade -y
-apt-get install sudo xorg make git -y
+apt-get install sudo xorg make -y
 sed -i '20i'$user' ALL=(ALL:ALL) ALL' /etc/sudoers
 
+#Setup wifi
 echo "Setup WiFi? [y/n]"
 read wifi
 if [ "$wifi" = "y" ]; then
@@ -35,12 +38,16 @@ echo 'auth_alg=OPEN' >> /etc/wpa_supplicant/wpa_supplicant.conf
 echo '}' >> /etc/wpa_supplicant/wpa_supplicant.conf
 fi
 
+#Install dependencies for Suckless
 apt-get install gcc libx11-dev libxft-dev libxinerama-dev fonts-font-awesome -y
 
+#Cloning the github repos
 git clone https://www.github.com/MatthiasBenaets/dwm /home/$user/.dwm
 git clone https://www.github.com/MatthiasBenaets/st /home/$user/.st
 git clone https://git.suckless.org/dmenu /home/$user/.dmenu
+git clone https://www.github.com/MatthiasBenaets/dotfiles /home/$user/.dotfiles
 
+#Install Suckless software
 cd /home/$user/.dwm
 make clean install
 cd /home/$user/.st
@@ -48,20 +55,21 @@ make clean install
 cd /home/$user/.dmenu
 make clean install
 
+#Startup files
 echo 'startx' >> /etc/profile
 touch /home/$user/.xinitrc
 echo 'exec dwm' >> /home/$user/.xinitrc
 
-apt-get install alsa-utils pulseaudio pavucontrol -y
-pulseaudio --check
-pulseaudio -D
+#Install sound packages
+apt-get install alsa-utils pulseaudio pulsemixer -y
 
+#Install bluetooth packages w/ auto-switch
 apt-get install bluez blueman -y
 sed -i '67iload-module module-switch-on-connect' /etc/pulse/default.pa
-pulseaudio -k
 
+#Setting up touchpad
 touch /etc/X11/xorg.conf.d/70-synaptics.conf
-echo 'Section "InputClass' >> /etc/X11/xorg.conf.d/70-synaptics.conf
+echo 'Section "InputClass"' >> /etc/X11/xorg.conf.d/70-synaptics.conf
 echo 'Identifier "touchpad"' >> /etc/X11/xorg.conf.d/70-synaptics.conf
 echo 'Driver "synaptics"' >> /etc/X11/xorg.conf.d/70-synaptics.conf
 echo 'MatchIsTouchpad "on"' >> /etc/X11/xorg.conf.d/70-synaptics.conf
@@ -69,18 +77,30 @@ echo 'Option "Tapping" "on"' >> /etc/X11/xorg.conf.d/70-synaptics.conf
 echo 'Option "NaturalScrolling" "on"' >> /etc/X11/xorg.conf.d/70-synaptics.conf
 echo 'EndSection' >> /etc/X11/xorg.conf.d/70-synaptics.conf
 
-echo "Is this a Virtual Machine? [y/n]"
-read vm
+#Install image viewer and compositor
 apt-get install feh compton -y
 mkdir /home/$user/Pictures
-sed -i '1ifeh --bg-center /Pictures/wall.jpg' /home/$user/.xinitrc
-sed -i '2icompton -f &' /home/$user/.xinitrc
-if [ "$vm" = "y" ]; then
-sed -i '1ixrandr --output Virtual1 --mode 1280x960' /home/$user/.xinitrc
-fi
+##sed -i '1ifeh --bg-center /Pictures/wall.jpg' /home/$user/.xinitrc
+##sed -i '2icompton -f &' /home/$user/.xinitrc
 
+#Install filemanager
 apt-get install ranger pip -y
 ranger --copy-config=all
 pip3 install ueberzug
+
+#Customization
+cp /home/$user/.dwm/resc/wall.jpg /home/$user/Pictures/
+cp /home/$user/.dwm/resc/sourcecodepro /usr/share/fonts/opentype/
+fc-cache -fv
+cp -f /home/$user/.dotfiles/{.xinitrc,.bashrc} /home/$user/
+cp -f /home/$user/.dotfiles/rc.conf /home/$user/.config/ranger/
+
+
+#Xrandr for automated resolution in vm
+echo "Is this a Virtual Machine? [y/n]"
+read vm
+if [ "$vm" = "y" ]; then
+sed -i '1ixrandr --output Virtual1 --mode 1280x960' /home/$user/.xinitrc
+fi
 
 sudo reboot
