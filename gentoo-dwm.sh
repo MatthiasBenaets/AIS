78df6116d8495984e7dcb588354b505f726f0064
MatthiasBenaets <matthias.benaets@gmail.com>
2021-10-12 23:14:14 +0200

    Fix xinitrc, patches dir, fonts

--- gentoo-dwm.sh
+++ gentoo-dwm.sh
@@ -1,6 +1,6 @@
 #Finalizing
 echo "Choose a username"
-read $user
+read user
 useradd -m -G users,wheel,audio,video,cdrom,usb -s /bin/bash $user
 echo "Choose a password"
 passwd $user
@@ -18,26 +18,33 @@ emerge -v x11-base/xorg-server x11-base/xorg-drivers
 env-update
 source /etc/profile
 
-emerge -v x11-wm/dwm x11-terms/st x11-misc/dmenu x11-apps/setxkbmap x11-apps/xrandr x11-misc/compton media-gfx/feh app-misc/ranger
+emerge -v x11-wm/dwm x11-terms/st x11-misc/dmenu x11-apps/setxkbmap x11-apps/xrandr x11-misc/compton media-gfx/feh app-misc/ranger media-fonts/fontawesome
 rc-update add elogind boot
 
+#Downloading git repo
 git clone https://www.github.com/MatthiasBenaets/dwm /home/$user/dwm
 git clone https://www.github.com/MatthiasBenaets/st /home/$user/st
 git clone https://www.github.com/MatthiasBenaets/dotfiles /home/$user/dotfiles
 
-cp /home/$user/dotfiles/.xinitrc ../.
+#Edit xinitrc
+sleep 1s
+cp /home/$user/dotfiles/.xinitrc /home/$user/ 
 sed -i '2isetxkbmap be' /home/$user/.xinitrc
 echo "Is this a Virtual Machine? [y/n]"
-read vr
-if [ "$vr" = "y" ]; then
+read vm
+if [ "$vm" = "y" ]; then
 	sed -i '2ixrandr --output Virtual-1 --mode 1280x960' /home/$user/.xinitrc 
 fi
 echo "startx" >> /etc/profile
 
-rm -rf /etc/portage/patches/x11-wm/dwm
+#Creating folders and copying everything over
+mkdir /etc/portage/patches/ /etc/portage/patches/x11-wm/ /etc/portage/patches/x11-terms/
 cp -r /home/$user/dwm/patches /etc/portage/patches/x11-wm/dwm
-rm -rf /etc/portage/patches/x11-terms/st
 cp -r /home/$user/st/patches /etc/portage/patches/x11-terms/st
+
+sed -i '/#include "shiftview.c"/d' /home/$user/dwm/config.def.h
+sed -i '/{ MODKEY,			XK_n,	   shiftview,	   {.i = +1 } },/d' /home/$user/dwm/config.def.h
+sed -i '/{ MODKEY,			XK_b,	   shiftview,	   {.i = -1 } },/d' /home/$user/dwm/config.def.h
 
 cp /home/$user/dwm/config.def.h /etc/portage/savedconfig/x11-wm/dwm-6.2
 ln -s /etc/portage/savedconfig/x11-wm/dwm-6.2 /etc/portage/savedconfig/x11-wm/dwm-6.2.h
@@ -50,10 +57,10 @@ cp /home/$user/dwm/resc/wall.jpg /home/$user/Pictures/
 su -c "ranger --copy-config=all" -s /bin/sh $user
 cp -f /home/$user/dotfiles/rc.conf /home/$user/.config/ranger/
 
-emerge dev-python/pip
-pip3 install ueberzug
+#Optional for ranger preview:
+#emerge dev-python/pip
+#pip3 install ueberzug
 
+#Rebuild and reboot
 emerge dwm st
-
 sudo reboot
-
